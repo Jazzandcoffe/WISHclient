5452dba55d321a830e1631889730831a7a05ac56
Jazzandcoffe <herman.molinder@gmail.com>
2014-04-04 15:49:55 +0200

    Lagt till portfunktioner

--- WISH client/Program.cs
+++ WISH client/Program.cs
@@ -9,17 +9,32 @@ using System.IO.Ports;
 namespace WISH_client
 {
     static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+    {   
         [STAThread]
         static void Main()
         {
-            
+            // Instantiate the communications
+            // port with some basic settings
+            SerialPort port = new SerialPort("COM3", 115200, Parity.None, 8, StopBits.One);
+
+            //Vad gör denna?
+            port.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+            //Öppna port
+            port.Open();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            port.Close();
+        }
+
+        //Vad gör denna?
+        private static void DataReceivedHandler( object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting();
         }
     }
 }
