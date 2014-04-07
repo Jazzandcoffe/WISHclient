ef45cb756e50309cbc74a7249199d451f31df480
Jazzandcoffe <herman.molinder@gmail.com>
2014-04-07 13:44:27 +0200

    keydown och keyup

--- WISH client/Form1.cs
+++ WISH client/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.IO.Ports;
+
 namespace WISH_client
 {
     public partial class Form1 : Form
@@ -17,38 +19,94 @@ namespace WISH_client
             InitializeComponent();
 
             this.KeyPreview = true;
-            this.KeyPress += new KeyPressEventHandler(Form1_KeyPress);
+            this.KeyDown += new KeyEventHandler(Form1_KeyDownEvent);
+            this.KeyUp += new KeyEventHandler(Form1_KeyUpEvent);
+
+            // Instantiate the communications
+            // port with some basic settings
+            SerialPort port = new SerialPort("COM3", 115200, Parity.None, 8, StopBits.One);
+
+            //Vad gör denna?
+            port.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+            //Öppna port
+            //port.Open();
         }
 
-        void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        void Form1_KeyDownEvent(object sender, KeyEventArgs e)
         {
-            switch (e.KeyChar)
+            switch (e.KeyCode)
             {
                 //q
-                case (char)113:
-
+                case Keys.Q:
+                    
                     break;
                 //w
-                case (char)119:
-
+                case Keys.W:
+                   
                     break;
                 //e
-                case (char)101:
+                case Keys.E:
 
                     break;
                 //a
-                case (char)97:
+                case Keys.A:
 
                     break;
                 //s
-                case (char)115:
+                case Keys.S:
 
                     break;
                 //d
-                case (char)100:
+                case Keys.D:
 
                     break;
             }
+        }
+
+        void Form1_KeyUpEvent(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                //q
+                case Keys.Q:
+                  
+                    break;
+                //w
+                case Keys.W:
+
+                    break;
+                //e
+                case Keys.E:
+
+                    break;
+                //a
+                case Keys.A:
+
+                    break;
+                //s
+                case Keys.S:
+
+                    break;
+                //d
+                case Keys.D:
+
+                    break;
+            }
+        }
+
+        //Tar emot data från seriell port.
+        private static void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            int indata1 = sp.ReadByte();
+            int indata2 = sp.ReadByte();
+            //konverterar decimalt -> hexadecimalt.
+            string hex1 = string.Format("{0:x}", indata1);
+            string hex2 = string.Format("{0:x}", indata2);
+
+            //Skriver ut mottagen data i konsolen
+            Console.WriteLine(hex1 + " " + hex2);
         }
 
         private void Form1_Load(object sender, EventArgs e)
--- WISH client/Program.cs
+++ WISH client/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-using System.IO.Ports;
+
 
 
 namespace WISH_client
@@ -14,35 +14,11 @@ namespace WISH_client
         [STAThread]
         static void Main()
         {
-            // Instantiate the communications
-            // port with some basic settings
-            SerialPort port = new SerialPort("COM3", 115200, Parity.None, 8, StopBits.One);
-
-            //Vad gör denna?
-            port.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
-
-            //Öppna port
-            //port.Open();
-
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
 
             //port.Close();
-        }
-
-        //Tar emot data från seriell port.
-        private static void DataReceivedHandler( object sender, SerialDataReceivedEventArgs e)
-        {
-            SerialPort sp = (SerialPort)sender;
-            int indata1 = sp.ReadByte();
-            int indata2 = sp.ReadByte();
-            //konverterar decimalt -> hexadecimalt.
-            string hex1 = string.Format("{0:x}", indata1);
-            string hex2 = string.Format("{0:x}", indata2);
-
-            //Skriver ut mottagen data i konsolen
-            Console.WriteLine(hex1 + " " + hex2);
         }
     }
 }
