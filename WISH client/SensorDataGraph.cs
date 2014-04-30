f5f4078c0cfda78de0fa36ad0a300155c9a16e67
robho345 <robho345@student.liu.se>
2014-04-30 13:26:20 +0200

    Lagt till klass: SensorDataGraph

---
+++ WISH client/SensorDataGraph.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WISH_client
+{
+    
+
+    /// <summary>
+    /// Klass som listan fylls med som utgör datapunkterna i grafen. 
+    /// </summary>
+    public class SensorDataGraph
+    {
+        private int x;
+        private int y;
+
+        public SensorDataGraph(int valueX, int valueY)
+        {
+            X = valueX;
+            Y = valueY;
+        }
+
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+    }
+}
