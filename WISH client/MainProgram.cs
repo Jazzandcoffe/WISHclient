5debfdd57be12ef327f517db04b0f00a0b75ec69
Jazzandcoffe <herman.molinder@gmail.com>
2014-05-05 17:57:49 +0200

    Kommenterat bort bugg
    När denna funktion körs säger GUI:t att kontakten med bluetoothen
    förloras. Jag har kommenterat bort anropet förtillfället.

--- WISH client/MainProgram.cs
+++ WISH client/MainProgram.cs
@@ -232,7 +232,7 @@ namespace WISH_client
             lblRearDetect.Text = dataOfType[12].ToString();
             lblDistRight.Text = dataOfType[13].ToString();
             lblDistLeft.Text = dataOfType[14].ToString();
-            updateTxtBox(dataOfType[32].ToString()); //Ändra typ efter protokoll  ((((EJ implementerad i Styr))))
+            //updateTxtBox(dataOfType[32].ToString()); //Ändra typ efter protokoll  ((((EJ implementerad i Styr))))
             AddValueToChart(ref _dataFront, dataOfType[9]);
             AddValueToChart(ref _dataRight, dataOfType[13]);
             AddValueToChart(ref _dataBack, dataOfType[10]);
